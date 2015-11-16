30352bec3e0d8ded72b05dcb2480483a01ed8a8c
Carlos Macasaet <github@carlos-m.net>
2015-11-15 22:39:46 -0800

    Register app in SNS

--- Push Notification App/AppDelegate.swift
+++ Push Notification App/AppDelegate.swift
@@ -14,11 +14,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // view
     var window: UIWindow?
 
-    // controller
+    // MARK: services
+    var sns:AWSSNS!
+    lazy var serviceManager = AWSServiceManager.defaultServiceManager()
+    lazy var bundle = NSBundle.mainBundle()
 
-    // MARK: delegate methods
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
-    {
+    // MARK: properties
+    /// private settings
+    var settings:Dictionary<String, AnyObject>!
+    var pushNotificationsEnabled = false
+    /// the APNS device token iff the user opted-in to notifications
+    var deviceToken:String! // set by application didRegisterForRemoteNotificationsWithDeviceToken
+    /// The endpoint is specific to this device and the application in AWS SNS
+    var endpointArn:String?
+
+    var userNotificationCategories:Set<UIUserNotificationCategory> = {
         let readAction = UIMutableUserNotificationAction() // value type fail!
         readAction.identifier = "READ_ACTION"
         readAction.title = "Read"
@@ -45,9 +55,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         category.setActions([ readAction, ignoreAction, deleteAction ], forContext: .Default )
         category.setActions( [ readAction, ignoreAction ], forContext: .Minimal )
 
-        let notificationSettings = UIUserNotificationSettings( forTypes: [ .Alert, .Badge, .Sound ], categories: [ category ] )
-        application.registerForRemoteNotifications()
-        application.registerUserNotificationSettings(notificationSettings)
+        return [ category ]
+    }()
+
+    // MARK: delegate methods
+    func application( application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]? ) -> Bool
+    {
+        /* AWS Configuration */
+        settings =
+            NSDictionary( contentsOfFile: self.bundle.pathForResource( "private", ofType: "plist" )! ) as! Dictionary<String, AnyObject>
+        let regionType = AWSRegionType( rawValue: settings[ "aws_region" ] as! Int )!
+        let identityPoolId = settings[ "aws_identity_pool_id" ] as! String
+
+        // configure Cognito
+        let credentialsProvider =
+            AWSCognitoCredentialsProvider( regionType: regionType, identityPoolId: identityPoolId )
+        let serviceConfiguration =
+            AWSServiceConfiguration( region: regionType, credentialsProvider: credentialsProvider )
+        serviceManager.defaultServiceConfiguration = serviceConfiguration
+
+        // configure SNS
+        AWSSNS.registerSNSWithConfiguration( serviceConfiguration, forKey: "SNS" )
+        sns = AWSSNS( forKey: "SNS" )
+
+        registerForNotifications( application )
 
         if let options = launchOptions
         {
@@ -57,20 +88,129 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
+    internal func registerForNotifications( application: UIApplication )
+    {
+        let notificationSettings =
+            UIUserNotificationSettings( forTypes: [ .Alert, .Badge, .Sound ],
+                                      categories: userNotificationCategories )
+        application.registerForRemoteNotifications()
+        application.registerUserNotificationSettings( notificationSettings )
+    }
+
     // MARK: Notification Delegates
-    func application( application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData ) {
+    func application( application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData )
+    {
         NSLog( "deviceToken: %@", deviceToken );
+        self.deviceToken =
+            deviceToken.description.stringByTrimmingCharactersInSet( NSCharacterSet(charactersInString: "<>") ).stringByReplacingOccurrencesOfString(" ", withString: "")
+
+        var taskChain = AWSTask( result: nil )
+        if( endpointArn == nil )
+        {
+            NSLog( "endpoint not yet set, scheduling registration with application" )
+            // register this session as an endpoint to the application
+            taskChain = taskChain.continueWithSuccessBlock()
+            {
+                task in
+
+                let request = AWSSNSCreatePlatformEndpointInput()
+                request.token = self.deviceToken
+                request.platformApplicationArn = self.settings[ "aws_sns_sandbox_arn" ] as! String
+
+                NSLog( "registering endpoint with application" )
+                // duplicated code, 1st time
+                return self.sns.createPlatformEndpoint( request ).continueWithSuccessBlock()
+                {
+                    task in
+                    NSLog( "registration successful, setting endpointArn" )
+                    let result = task.result as! AWSSNSCreateEndpointResponse
+                    self.endpointArn = result.endpointArn
+                    NSLog( "endpointArn: " + self.endpointArn! )
+                    return task
+                }
+            }
+        }
+        taskChain.continueWithSuccessBlock()
+        {
+            task in
+            NSLog( "Getting endpoint attributes for: \(self.endpointArn)" )
+            let request = AWSSNSGetEndpointAttributesInput()
+            request.endpointArn = self.endpointArn
+            return self.sns.getEndpointAttributes( request )
+        }.continueWithBlock()
+        {
+            task in
+            if let error = task.error
+            {
+                NSLog( "Error getting endpoint attributes: \(error.description)" )
+                if error.domain == AWSSNSErrorDomain && AWSSNSErrorType( rawValue: error.code ) == .NotFound
+                {
+                    NSLog( "Endpoint was deleted, need to re-register" )
+                    // endpoint was deleted, need to re-register
+                    let request = AWSSNSCreatePlatformEndpointInput()
+                    request.token = self.deviceToken
+                    request.platformApplicationArn = self.settings[ "aws_sns_sandbox_arn" ] as! String
+
+                    // duplicated code, 2nd time
+                    return self.sns.createPlatformEndpoint( request ).continueWithSuccessBlock()
+                    {
+                        task in
+                        let result = task.result as! AWSSNSCreateEndpointResponse
+                        self.endpointArn = result.endpointArn
+                        NSLog( "endpointArn: " + self.endpointArn! )
+                        return task
+                    }
+                }
+            }
+            else if let result = task.result
+            {
+                let response = result as! AWSSNSGetEndpointAttributesResponse
+                if (response.attributes[ "Token" ] as! String) != self.deviceToken || response.attributes[ "Enabled" ] as! String == "false"
+                {
+                    NSLog( "Endpoint registration out of date, updating" )
+                    let updateRequest = AWSSNSSetEndpointAttributesInput()
+                    updateRequest.endpointArn = self.endpointArn!
+                    updateRequest.attributes = response.attributes // ooh, copy on write
+                    updateRequest.attributes[ "Token" ] = self.deviceToken
+                    updateRequest.attributes[ "Enabled" ] = "true"
+                    return self.sns.setEndpointAttributes( updateRequest )
+                }
+            }
+            return task
+        }.continueWithBlock()
+        {
+            task in
+            if let error = task.error
+            {
+                NSLog( "Error registering for push notifications: \(error.description)" )
+                self.createAlert( "Error: \(error.description)" )
+            }
+            else if let exception = task.exception
+            {
+                NSLog( "Exception registering for push notifications: \(exception.description)" )
+                self.createAlert( "Exception: \(exception.description)" )
+            }
+            else
+            {
+                NSLog( "Registered for push notifications!" )
+                self.pushNotificationsEnabled = true
+            }
+            return task
+        }
     }
 
     func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
         NSLog( "unable to register for remote notifications: %@", error )
+        pushNotificationsEnabled = false
     }
 
     func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        NSLog( "Received notification: \(userInfo.description)" )
         createAlert( userInfo.description )
     }
 
     func application(application: UIApplication, handleActionWithIdentifier identifier: String?, forRemoteNotification userInfo: [NSObject : AnyObject], completionHandler: () -> Void) {
+        NSLog( "Handling notification action: \(identifier)" )
         guard let identifier = identifier else
         {
             completionHandler()
@@ -100,4 +240,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 }
-
---
+++ Push Notification App/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  Push Notification App
+//
+//  Created by Carlos Macasaet on 15/11/15.
+//  Copyright © 2015 Carlos Macasaet. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <AWSCore/AWSCore.h>
+#import <AWSSNS/AWSSNS.h>
+
+#endif /* Bridging_Header_h */
